88fe559fe1c46fa91357ea22af17d45ff24a13b4
riskanuris2 <41239255+riskanuris2@users.noreply.github.com>
2020-05-14 06:35:57 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,7 +1,6 @@
 // prettier-ignore
 export default [
     { author: "Riska Kaosei", quotes: "Whatever happens, keep breathing" },
-    { author: "Riska Kaosei", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
     { author: "Riska Kaosei", quotes: "Hey! I wuv chuuuu! UwU" },
     { author: "Riska Kaosei", quotes: "I love you." },
     { author: "Riska Kaosei", quotes: "I may not with you everyday, but I love you everyday." },
