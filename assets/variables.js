6ea4dd392a545d7a24096fea8f0d65b22a684584
riskanuris2 <41239255+riskanuris2@users.noreply.github.com>
2020-05-14 23:22:00 +0700

    ava

---
+++ assets/variables.js
@@ -0,0 +1,10 @@
+export default {
+  nicknames: ["Beb", "Babe", "Sayang", "Zeyenk", "Cintuu"],
+  greetings: {
+    evening: "Good Evening",
+    afternoon: "Good Afternoon",
+    day: "Good Day",
+    morning: "Good Morning",
+    night: "Good Night"
+  }
+};
---
+++ config.js
@@ -0,0 +1,5 @@
+const config = {
+  host: "localhost", // default host
+  port: 5500 // default port
+};
+export default config;
