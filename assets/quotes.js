e5e2056f2bd2a7fc2f30e865d18486d99ff6a8e9
riskanuris2 <41239255+riskanuris2@users.noreply.github.com>
2021-02-13 12:00:30 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,9 +1,4 @@
 // prettier-ignore
 export default [
-    { author: "Riska Kaosei", quotes: "Hey Beb" },
-    { author: "Riska Kaosei", quotes: "jane Pengen Tak Omongne Nek Ketemuan" },
-    { author: "Riska Kaosei", quotes: "But you Know" },
-    { author: "Riska Kaosei", quotes: "Wes Pirang Tahun Dewe Cedak" },
-    { author: "Riska Kaosei", quotes: "I love you." },
-    { author: "Riska Kaosei", quotes: "Gelem Gak Dadi Pacarku?" },
+    { author: "Riska Kaosei", quotes: "Woco Sampe habis, Jawab Lewat Telpon Yak :D" },
 ];
