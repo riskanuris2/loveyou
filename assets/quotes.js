749312fb25e4041f2a21f2d732fd046f469102b5
riskanuris2 <41239255+riskanuris2@users.noreply.github.com>
2021-02-28 17:43:26 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,5 +1,6 @@
 // prettier-ignore
 export default [
     { author: "Riska Kaosei", quotes: "Good Day" },
-    { author: "Riska Kaosei", quotes: "Have A Nice Dream :D" }
+    { author: "Riska Kaosei", quotes: "Have A Nice Day :D" },
+    { author: "Riska Kaosei", quotes: "Good Luck" },
 ];
